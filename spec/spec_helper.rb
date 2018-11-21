0e2b74f3671fc66ec5b9b8a76c19f6a97cb463c2
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-21 21:27:16 +0000

    Included a path to the even_fibonacci_numbers in the require_relative section

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require_relative "../lib/fizz_buzz"
 require_relative "../lib/sum_of_multiples"
+require_relative "../lib/even_fibonacci_numbers"
 
 RSpec.configure do |config|
   config.formatter = :documentation
