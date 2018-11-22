1d3328180afdf2c846053ede2470f4cddb324c69
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-22 14:00:29 +0000

    Commented out this file for rspec to test another file

---
+++ spec/test_fibonacci_spec.rb
@@ -0,0 +1,26 @@
+# describe "project euler problem 2" do
+#
+#   before(:each)do
+#     @fibonacci = Fibonacci.new
+#   end
+#
+#   it "should contain array of [1,2,3]" do
+#     expect(@fibonacci.fib_array).to match([1, 2, 3])
+#   end
+#
+#   it "should respond with total of 231 in the first 10 terms of the fibonacci sequence" do
+#     @fibonacci.fib_creator(10)
+#     expect(@fibonacci.fibonacci_sum).to eq 231
+#   end
+#
+#   it "should respond 44 for the sum of all even numbers from a fibonacci max of 10" do
+#     @fibonacci.fib_creator(10)
+#     expect(@fibonacci.even_number_total_value).to eq 44
+#   end
+#
+#
+#
+#
+#
+#
+# end
