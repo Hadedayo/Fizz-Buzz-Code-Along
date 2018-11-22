3bf2a85c6dceee83abc7fb219ee07297d7dbb340
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-22 14:01:44 +0000

    Included require relative for fibonacci walkthrough file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/fizz_buzz"
 require_relative "../lib/sum_of_multiples"
 require_relative "../lib/even_fibonacci_numbers"
+require_relative "../lib/fibonacci_walkthrough"
 
 RSpec.configure do |config|
   config.formatter = :documentation
