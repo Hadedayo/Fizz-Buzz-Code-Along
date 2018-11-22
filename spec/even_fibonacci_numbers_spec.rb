7ea9eca2419d61d0c5dfc58b1ac4505b110809ab
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-22 14:02:09 +0000

    Commented out this file

--- spec/even_fibonacci_numbers_spec.rb
+++ spec/even_fibonacci_numbers_spec.rb
@@ -1,22 +1,22 @@
-describe "fibonacci_numbers" do
-
-  before(:all) do
-    @fn = FibonacciNumbers.new
-  end
-
-  it "should show the first two numbers in the initial array" do
-    print @fn.fibonacciNumbersArray
-  end
-
-  it "should show the fibonacci sequence in an array" do
-    @fn.fibonacci(4000000)
-    print @fn.fibonacciNumbersArray
-  end
-
-  it "should return even numbers in an array" do
-  #     expect(@fb.divisible_by?(15,3)).to be true
-  #   end
-
-
-
-end
+# describe "fibonacci_numbers" do
+#
+#   before(:all) do
+#     @fn = FibonacciNumbers.new
+#   end
+#
+#   it "should show the first two numbers in the initial array" do
+#     print @fn.fibonacciNumbersArray
+#   end
+#
+#   it "should show the fibonacci sequence in an array" do
+#     @fn.fibonacci(4000000)
+#     print @fn.fibonacciNumbersArray
+#   end
+#
+#   it "should return even numbers in an array" do
+#   #     expect(@fb.divisible_by?(15,3)).to be true
+#   #   end
+#
+#
+#
+# end
