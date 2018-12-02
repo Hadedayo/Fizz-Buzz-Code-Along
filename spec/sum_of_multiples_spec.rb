0718018ebeac0135475c1713d261a0b71a2eb2d2
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-12-02 21:49:30 +0000

    Modified readme and deleted some files

--- spec/sum_of_multiples_spec.rb
+++ spec/sum_of_multiples_spec.rb
@@ -1,32 +1,32 @@
-require "spec_helper"
-  describe "multiples" do
-
-    before(:all) do
-      @mp = SumofMultiples.new
-    end
-
-
-    it "should return true if number is a multiple of 3" do
-      expect(@mp.multiple_of?(3,3)).to be true
-    end
-
-    it "should return false if number is not a multiple of 3" do
-      expect(@mp.multiple_of?(5,3)).to be false
-    end
-
-    it "should return true if number is a multiple of 5" do
-      expect(@mp.multiple_of?(25,5)).to be true
-    end
-
-    it "should return false if number is not a multiple of 5" do
-      expect(@mp.multiple_of?(31,5)).to be false
-    end
-
-    it "should correctly show the SUM of the multiples of 3 or 5 between the range 1 and 1000" do
-      @mp.multiples_iterator(1,1000)
-      expect(@mp.sum_of_multiples).to eq 233168
-    end
-
-
-
-  end
+# require "spec_helper"
+#   describe "multiples" do
+#
+#     before(:all) do
+#       @mp = SumofMultiples.new
+#     end
+#
+#
+#     it "should return true if number is a multiple of 3" do
+#       expect(@mp.multiple_of?(3,3)).to be true
+#     end
+#
+#     it "should return false if number is not a multiple of 3" do
+#       expect(@mp.multiple_of?(5,3)).to be false
+#     end
+#
+#     it "should return true if number is a multiple of 5" do
+#       expect(@mp.multiple_of?(25,5)).to be true
+#     end
+#
+#     it "should return false if number is not a multiple of 5" do
+#       expect(@mp.multiple_of?(31,5)).to be false
+#     end
+#
+#     it "should correctly show the SUM of the multiples of 3 or 5 between the range 1 and 1000" do
+#       @mp.multiples_iterator(1,1000)
+#       expect(@mp.sum_of_multiples).to eq 233168
+#     end
+#
+#
+#
+#   end
